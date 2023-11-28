2e0149b9a6b1856f977236decabbc25ada5acb99
Binye Barwe <binyebarwe@gmail.com>
2023-11-28 14:14:27 -0500

    do not use useconnection from wallet adapter

--- hooks/useProvider.ts
+++ hooks/useProvider.ts
@@ -1,15 +1,16 @@
 import { AnchorProvider } from "@coral-xyz/anchor";
 import { useMemo } from "react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useFakeProvider } from "./useOpenbookClient";
 
 export function useProvider() {
-  const { connection } = useConnection();
-  const wallet = useWallet();
+  // const { connection } = useConnection();
+  // const wallet = useWallet();
 
-  const provider = useMemo(
-    () => new AnchorProvider(connection, wallet as any, {}),
-    [connection, wallet]
-  );
+  // const provider = useMemo(
+  //   () => new AnchorProvider(connection, wallet as any, {}),
+  //   [connection, wallet]
+  // );
 
-  return provider;
+  return useFakeProvider();
 }
